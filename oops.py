23e40bc1fbc782ab142917ac8925f68e692ad6a0
phanikumarinampudi <77920639+phanikumarinampudi@users.noreply.github.com>
2021-02-11 18:42:50 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,13 @@
+"""this module consists of all the arithmetic operations"""
+def Add(*a):
+    """this function will return additional value"""
+    return sum(a)
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
